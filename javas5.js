615960da0dceecf05433ba6e388983176468b7da
facundoMunoz084 <munozfacundo706@gmail.com>
2022-11-07 17:47:35 -0300

    se agrega el proyecto

---
+++ javas5.js
@@ -0,0 +1,16 @@
+
+//funciones de crear 
+var crearPersona = [
+    {id:"1022", nombre:"facundo", apellido:"muñoz", telefono:"199893", ciodad:"quilaco", direccion:"artu1023"},
+    {id:"3444", nombre:"carlost", apellido:"meta", telefono:"999999", ciodad:"santa adr", direccion:"rodrigues1223"},
+    {id:"7777", nombre:"federico", apellido:"latros", telefono:"555567", ciodad:"adrianasan ", direccion:"mata2033"}
+
+
+];
+
+console.log(crearPersona)
+
+//funciones de eliminar 
+
+var removed = crearPersona .splice(2);
+console.log(crearPersona);
